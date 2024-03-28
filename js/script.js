ab939a3c63f9289f195e62a86ee46cb489407fe1
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-03-28 12:09:31 +0100

    Add: function to show image on thumbnails click - Bonus 1

--- js/script.js
+++ js/script.js
@@ -51,6 +51,19 @@ createApp({
             } else {
                 this.activeImage = this.slides.length - 1;
             }
+        },
+
+        //MOSTRO IMMAGINE DELLA THUMBNAIL CLICCATA 
+        showImage: function(clickedImage) {
+            this.activeImage = clickedImage;
         }
     }
 }).mount('#app');
+
+
+// Bonus:
+// 1- al click su una thumb, visualizzare in grande l'immagine corrispondente
+// 2- applicare l'autoplay allo slider: ogni 3 secondi, 
+// cambia immagine automaticamente
+// 3- quando il mouse va in hover sullo slider, 
+// bloccare l'autoplay e farlo riprendere quando esce
