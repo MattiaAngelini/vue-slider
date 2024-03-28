a75b488820f131928758290327ceb0f63d719974
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-03-28 10:22:16 +0100

    Add: methods for nextImage & prevImage

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-
-// Descrizione:
-// Partendo dal markup fornito nello starter pack, rifare lo slider ma questa volta usando Vue.
-// Bonus:
-// 1- al click su una thumb, visualizzare in grande l'immagine corrispondente
-// 2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
-// 3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
-
 const { createApp } = Vue;
 
 createApp({
@@ -41,6 +33,22 @@ createApp({
             activeImage: 0
         };
     },
+    
     methods: {
+        nextImage: function() {         
+            if (this.activeImage < this.slides.length - 1) {
+                this.activeImage++;
+            } else {
+                this.activeImage = 0;
+            }
+        },
+
+        prevImage: function() {
+            if (this.activeImage > 0) {
+                this.activeImage--;
+            } else {
+                this.activeImage = this.slides.length - 1;
+            }
+        }
     }
 }).mount('#app');
